5d8ed7303757913399cde2632e7aa69852535e24
Daniel Ziltener <dziltener@lyrion.ch>
2013-08-28 21:05:12 +0200

    Added maps as form of arguments for the build macro.

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject clojurefx "0.1.0-SNAPSHOT"
-  :description "Helper functions and probably a wrapper to simplify usage of JavaFX in Clojure.<br/>
-You'll need to have jfxrt.jar in your local maven repository. See [this coderwall protip](https://coderwall.com/p/4yjy1a) for how to make this happen."
+  :description "Helper functions and probably a wrapper to simplify usage of JavaFX in Clojure.
+
+You'll need to have jfxrt.jar in your local maven repository. See [this coderwall protip](https://coderwall.com/p/4yjy1a) for how to make this happen.
+
+**This is not yet on clojars, but it will be there soon. Until then, clone it and install locally.**"
   :url "https://www.github.com/zilti/clojurefx"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
