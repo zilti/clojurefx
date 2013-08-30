ceb113490a2aabf2a8edfc0e80aeef858d7c7a03
Daniel Ziltener <dziltener@lyrion.ch>
2013-08-30 12:53:36 +0200

    First clojars release

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
 
 You'll need to have jfxrt.jar in your local maven repository. See [this coderwall protip](https://coderwall.com/p/4yjy1a) for how to make this happen.
 
-**This is not yet on clojars, but it will be there soon. Until then, clone it and install locally.**"
+**Installation: `[clojurefx \"0.0.1\"]`**"
   :url "https://www.github.com/zilti/clojurefx"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
