3a2462bbe1d473989b73eee5b6681a4a478f3f58
Daniel Ziltener <dziltener@lyrion.ch>
2013-08-31 14:04:06 +0200

    Fixed.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clojurefx "0.0.2"
+(defproject clojurefx "0.0.3"
   :description "Helper functions and probably a wrapper to simplify usage of JavaFX in Clojure.
 
 You'll need to have jfxrt.jar in your local maven repository. See [this coderwall protip](https://coderwall.com/p/4yjy1a) for how to make this happen.
 
-**Installation: `[clojurefx \"0.0.2\"]`**"
+**Installation: `[clojurefx \"0.0.3\"]`**"
   :url "https://www.github.com/zilti/clojurefx"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
