c767518f34ba1cfb1c72a41549636e3babef4a01
Daniel Ziltener <dziltener@lyrion.ch>
2013-11-03 00:14:13 +0100

    lein-release plugin: bumped version from 0.0.4 to 0.0.5-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject clojurefx "0.0.3"
+(defproject clojurefx "0.0.5-SNAPSHOT"
   :description "Helper functions and probably a wrapper to simplify usage of JavaFX in Clojure.
 
 You'll need to have jfxrt.jar in your local maven repository. See [this coderwall protip](https://coderwall.com/p/4yjy1a) for how to make this happen.
 
-**Installation: `[clojurefx \"0.0.3\"]`**"
+**Installation: `[clojurefx \"0.0.4\"]`**"
   :url "https://www.github.com/zilti/clojurefx"
+  :lein-release {:deploy-via :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.oracle/javafx-runtime "2.2.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[lein-marginalia "0.7.1"]])
