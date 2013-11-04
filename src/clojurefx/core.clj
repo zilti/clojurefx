65b12e6533740d967f29cd85109c3ef0fbb4a05c
Daniel Ziltener <zilti@lyrion.ch>
2013-11-04 17:10:19 +0100

    Added the new JavaFX 8 component names

--- src/clojurefx/core.clj
+++ src/clojurefx/core.clj
@@ -185,13 +185,14 @@ The listener gets a preprocessed event map as shown above.
     (apply str in)))
 
 (def pkgs (atom {"javafx.scene.control" '[accordion button cell check-box check-box-tree-item check-menu-item choice-box
-                                          color-picker combo-box context-menu custom-menu-item hyperlink
+                                          color-picker combo-box context-menu custom-menu-item date-picker date-cell hyperlink
                                           indexed-cell index-range label list-cell list-view menu-bar menu menu-button menu-item
                                           pagination password-field popup-control progress-bar progress-indicator
                                           radio-button radio-menu-item scroll-bar scroll-pane separator
                                           separator-menu-item slider split-menu-button split-pane tab table-cell table-column
                                           table-position table-row table-view tab-pane text-area text-field tree-cell
-                                          titled-pane toggle-button toggle-group tool-bar tooltip tree-item tree-view]
+                                          titled-pane toggle-button toggle-group tool-bar tooltip tree-item tree-view
+                                          tree-table-cell tree-table-column tree-table-row tree-table-view]
                  "javafx.scene.control.cell" '[check-box-list-cell check-box-table-cell check-box-tree-cell
                                                choice-box-list-cell choice-box-table-cell choice-box-tree-cell
                                                combo-box-list-cell combo-box-table-cell combo-box-tree-cell
@@ -199,10 +200,10 @@ The listener gets a preprocessed event map as shown above.
                                                property-value-factory]
                  "javafx.scene.layout" '[anchor-pane border-pane column-constraints flow-pane grid-pane h-box pane region
                                          row-constraints stack-pane tile-pane v-box]
-                 "javafx.scene.text" '[text font]
+                 "javafx.scene.text" '[text font text-flow]
                  "javafx.scene.shape" '[arc arc-to circle close-path cubic-curve cubic-curve-to ellipse
                                         h-line-to line line-to move-to path polygon polyline quad-curve quad-curve-to
-                                        rectangle SVG-path]
+                                        rectangle SVG-path box cylinder mesh-view sphere]
                  "javafx.scene.canvas" '[canvas]
                  "javafx.scene.image" '[image-view]
                  "javafx.scene.input" '[clipboard-content key-character-combination key-code-combination mnemonic]
@@ -216,7 +217,8 @@ The listener gets a preprocessed event map as shown above.
                  "javafx.scene.media" '[audio-clip media media-player media-view]
                  "javafx.scene.transform" '[affine rotate scale shear translate]
                  "javafx.scene.web" '[HTML-editor prompt-data web-engine web-view]
-                 "javafx.scene" '[scene group image-cursor perspective-camera snapshot-parameters]
+                 "javafx.scene" '[scene group image-cursor perspective-camera snapshot-parameters
+                                  ambient-light parallel-camera perspective-camera point-light]
                  "javafx.animation" '[fade-transition fill-transition parallel-transition path-transition pause-transition
                                       rotate-transition scale-transition sequential-transition stroke-transition timeline
                                       translate-transition]
