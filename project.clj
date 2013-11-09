1c9e46ab55ed1717404c1ccabc333a9812db8f8e
Daniel Ziltener <dziltener@lyrion.ch>
2013-11-09 13:07:05 +0100

    lein-release plugin: preparing 0.0.8 release

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clojurefx "0.0.7"
+(defproject clojurefx "0.0.8"
   :description "Helper functions and probably a wrapper to simplify usage of JavaFX in Clojure.
 
   This is meant to be used with Java 8. If you add JavaFX 2.2 to your classpath it might still work, but that isn't tested.
   
   [This Project On GitHub](https://www.github.com/zilti/clojurefx)
 
-**Installation: `[clojurefx \"0.0.7\"]`**"
+**Installation: `[clojurefx \"0.0.8\"]`**"
   :url "https://www.github.com/zilti/clojurefx"
   :lein-release {:deploy-via :clojars}
   :license {:name "Eclipse Public License"
