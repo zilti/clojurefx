6ede72fdb28fe1ed6a50bb77ffa0a7b2cf770ee3
Daniel Ziltener <dziltener@lyrion.ch>
2013-11-10 18:55:49 +0100

    lein-release plugin: bumped version from 0.0.9 to 0.0.10-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurefx "0.0.9"
+(defproject clojurefx "0.0.10-SNAPSHOT"
   :description "Helper functions and probably a wrapper to simplify usage of JavaFX in Clojure.
 
   This is meant to be used with Java 8. If you add JavaFX 2.2 to your classpath it might still work, but that isn't tested.
