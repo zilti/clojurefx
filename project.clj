479f6c17d02a4871f61ad921a529828e6e1af718
Efrain <efrain.bergillos@gmail.com>
2015-03-31 15:44:08 +0100

    Added bind-simple-property in order to make it work with javafx.beans.property.SimpleStringProperty Added some swappers and constructors Added style-classes to the fx builder Bumped the version to 0.0.14

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clojurefx "0.0.13"
+(defproject clojurefx "0.0.14"
   :description "Helper functions and probably a wrapper to simplify usage of JavaFX in Clojure.
 
   This is meant to be used with Java 8. If you add JavaFX 2.2 to your classpath it might still work, but that isn't tested.
   
   [This Project On GitHub](https://www.github.com/zilti/clojurefx)
 
-**Installation: `[clojurefx \"0.0.13\"]`**
+**Installation: `[clojurefx \"0.0.14\"]`**
 
 Navigation
 ----------
@@ -18,7 +18,7 @@ Navigation
   :lein-release {:deploy-via :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :plugins [[lein-marginalia "0.7.1"]
             [lein-midje "3.1.3-RC2"]
             [lein-release "1.0.5"]]
