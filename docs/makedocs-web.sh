9815f83013ef967343ecdaf7b76c5852e76b0415
zilti <zilti>
2017-12-18 21:28:34 +0000

    Adjusted makedocs-web.sh for automatic documentation generation.

--- docs/makedocs-web.sh
+++ docs/makedocs-web.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+cd docs
 makeinfo --css-ref=datamash-texinfo.css --html --no-split manual.texinfo
+echo '0a
+<div class="fossil-doc" data-title="Title Text">
+.
+$a
+</div>
+.
+w' | ed clojurefx.html
